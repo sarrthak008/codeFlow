48eb6a0a6ecec5a613106e9098722d0f8ad6ef61
sarrthak008 <sarthaknavale08@gmail.com>
2026-06-24 20:37:56 +0530

    add hosted client domian

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import cors from "cors"
 
 //middlewares
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173" , "https://codeefloww.vercel.app"],
     credentials: true,
 }))
 
